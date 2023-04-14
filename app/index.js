9b21439d9e2f5d4b4c97f149640e6891dfc93070
rana-arju <arjurana20@gmail.com>
2023-04-14 06:43:44 -0700

    getWork app header and welcome section design

---
+++ app/index.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { images, icons, COLORS, FONT, SIZES } from "../constants";
+import {
+  ScreenHeaderBtn,
+  Welcome,
+  Nearbyjobs,
+  Popularjobs,
+  Company,
+  JobTabs,
+  JobAbout,
+  JobFooter,
+  Specifics,
+  NearbyJobCard,
+} from "../components";
+const Home = () => {
+  const router = useRouter();
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+         
+          headerTitle: "Rana",
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome />
+          <Popularjobs />
+          <NearbyJobCard />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+export default Home;
---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import profile from "../assets/images/ranaarju.jpg";
+
+export default {
+  profile,
+};
